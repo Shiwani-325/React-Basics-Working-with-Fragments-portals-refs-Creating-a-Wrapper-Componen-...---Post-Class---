7c068f14ee55217e491df74200bc1b32be1f8d6f
Shiwani-325 <96593318+Shiwani-325@users.noreply.github.com>
2022-06-10 03:01:20 +0530

    Update SubHeading.js

--- src/components/SubHeading.js
+++ src/components/SubHeading.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SubmitButton() {
-  return <button>Ask</button>;
+function SubHeading() {
+  return <h5>Happy to solve you doubts.</h5>;
 }
-export default SubmitButton;
+export default SubHeading;
